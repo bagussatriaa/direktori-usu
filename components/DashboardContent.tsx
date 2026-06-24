e342b90ad37aa0bce60d2b2371ee0ae21d055577
bagussatriaa <satria.bsw03@gmail.com>
2026-06-24 11:22:53 +0700

    minor update --color, margin etc

--- components/DashboardContent.tsx
+++ components/DashboardContent.tsx
@@ -3,7 +3,7 @@ import {  Plus } from 'lucide-react';
 interface DashboardContentProps {
     title: string;
     description: string;
-    totCount: number;
+    totCount: string;
     imageSrc: string;
     isReverse?: boolean;
 };
@@ -15,13 +15,14 @@ export default function DashboardContent({ title, description, totCount, imageSr
       <div className="w-full md:w-1/2">
         <h2 className="text-2xl font-bold text-black-900 mb-4">{title}</h2>
         <p className="text-black mb-6 text-justify">{description}</p>
-        <div className="bg-green-50 p-4 rounded-xl inline-block">
+        <a href="#">
+        <div className="bg-[#192123] p-4 rounded-3xl inline-block hover:bg-[#2c393d] hover:text-white transition-colors duration-300">
           <div className="flex gap-1">
-              <span className="font-bold text-xl text-black-500">{totCount}</span>
-              <p className="mt-0.5">{title}</p>
+              {/* <span className="font-bold text-xl text-black-500">{totCount}</span> */}
+              <p className="mt-0.5 text-white">Cari {title}</p>
           </div>
           <div className="flex pt-2">
-                <img src="/dosen-konten.png" alt="Logo USU" className="w-6 h-6 ml-2"/>
+                <img src="/dosen-konten.png" alt="Logo USU" className="w-6 h-6"/>
                 <img src="/dosen-konten2.png" alt="Logo USU" className="w-6 h-6 ml-2"/>
                 <img src="/dosen-konten3.png" alt="Logo USU" className="w-6 h-6 ml-2"/>
                 <div className="bg-white ml-1 flex items-center justify-center rounded-full w-3 h-3">
@@ -29,6 +30,7 @@ export default function DashboardContent({ title, description, totCount, imageSr
                 </div>
           </div>
         </div>
+        </a>
       </div>
       
       {/* Kolom Gambar */}
