b6e7b94a306f9845a1b1155a1afabfa2cd5c0147
bagussatriaa <satria.bsw03@gmail.com>
2026-06-24 09:29:41 +0700

    updated layout, added icon

--- app/dev/content/page.tsx
+++ app/dev/content/page.tsx
@@ -10,20 +10,20 @@ export default function Home() {
            <Dashboard 
                 title="Dosen"
                 description="Dosen merupakan tenaga pendidik profesional yang berperan strategis dalam mendukung pelaksanaan Tri Dharma Perguruan Tinggi di Universitas Sumatera Utara, yaitu pendidikan, penelitian, dan pengabdian kepada masyarakat. Selain mengajar, dosen juga membimbing mahasiswa serta berkontribusi dalam pengembangan ilmu pengetahuan dan inovasi yang bermanfaat bagi masyarakat."
-                totcount={13000}
+                totCount={13000}
                 imageSrc="/dosen-dashboard.png"
             />
             <Dashboard 
                 isReverse={true}
                 title="Mahasiswa"
                 description="Mahasiswa merupakan bagian penting dari sivitas akademika Universitas Sumatera Utara yang berperan aktif dalam kegiatan pendidikan, pengembangan ilmu pengetahuan, serta berbagai aktivitas akademik dan kemahasiswaan. Sebagai insan pembelajar, mahasiswa tidak hanya mengikuti proses perkuliahan, tetapi juga mengembangkan potensi, keterampilan, dan kontribusi positif di lingkungan kampus maupun masyarakat."
-                totcount={100000}
+                totCount={100000}
                 imageSrc="/mahasiswa-dashboard.png"
             />            
             <Dashboard 
                 title="Tenaga Kependidikan"
                 description="Tenaga Kependidikan merupakan unsur penting yang mendukung kelancaran penyelenggaraan pendidikan di Universitas Sumatera Utara melalui layanan administrasi, akademik, dan operasional. Perannya mencakup penyediaan dukungan layanan yang efektif dan terintegrasi guna menunjang aktivitas dosen, mahasiswa, dan seluruh kebutuhan institusi."
-                totcount={10000}
+                totCount={10000}
                 imageSrc="/tendik-dashboard.png"
             />            
         </div>
--- components/DashboardContent.tsx
+++ components/DashboardContent.tsx
@@ -1,25 +1,32 @@
+import {  Plus } from 'lucide-react';
+
 interface DashboardContentProps {
     title: string;
     description: string;
-    totcount: number;
+    totCount: number;
     imageSrc: string;
     isReverse?: boolean;
 };
 
-export default function DashboardContent({ title, description, totcount, imageSrc, isReverse = false }: DashboardContentProps) {
+export default function DashboardContent({ title, description, totCount, imageSrc, isReverse = false }: DashboardContentProps) {
     return (
-    // md:flex-row-reverse bakal otomatis nukar posisi kiri-kanan berdasarkan props isReversed!
     <div className={`flex flex-col md:flex-row items-center gap-12 my-16 ${isReverse ? 'md:flex-row-reverse' : ''}`}>
       {/* Kolom Teks */}
       <div className="w-full md:w-1/2">
-        <h2 className="text-3xl font-bold text-green-900 mb-4">{title}</h2>
-        <p className="text-gray-600 mb-6 text-justify">{description}</p>
+        <h2 className="text-2xl font-bold text-black-900 mb-4">{title}</h2>
+        <p className="text-black mb-6 text-justify">{description}</p>
         <div className="bg-green-50 p-4 rounded-xl inline-block">
-          <span className="font-bold text-xl text-green-800">{totcount}</span> {title}
+          <div className="flex gap-1">
+              <span className="font-bold text-xl text-black-500">{totCount}</span>
+              <p className="mt-0.5">{title}</p>
+          </div>
           <div className="flex pt-2">
-                <img src="/logousu.png" alt="Logo USU" className="w-6 h-6 ml-2"/>
-                <img src="/logousu.png" alt="Logo USU" className="w-6 h-6 ml-2"/>
-                <img src="/logousu.png" alt="Logo USU" className="w-6 h-6 ml-2"/>
+                <img src="/dosen-konten.png" alt="Logo USU" className="w-6 h-6 ml-2"/>
+                <img src="/dosen-konten2.png" alt="Logo USU" className="w-6 h-6 ml-2"/>
+                <img src="/dosen-konten3.png" alt="Logo USU" className="w-6 h-6 ml-2"/>
+                <div className="bg-white ml-1 flex items-center justify-center rounded-full w-3 h-3">
+                  <Plus className="w-6 h-6"/>
+                </div>
           </div>
         </div>
       </div>
